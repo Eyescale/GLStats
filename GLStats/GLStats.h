aa4c79c43340dc069738ebebca9953752c8bda7c
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-01-10 11:24:51 +0100

    Add GLStats.h for convenience

---
+++ GLStats/GLStats.h
@@ -0,0 +1,29 @@
+
+/* Copyright (c) 2013, Stefan Eilemann <eile@eyescale.ch>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 2.1 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef GLSTATS_GLSTATS_H
+#define GLSTATS_GLSTATS_H
+
+#include <GLStats/data.h>
+#include <GLStats/entity.h>
+#include <GLStats/item.h>
+#include <GLStats/renderer.h>
+#include <GLStats/thread.h>
+#include <GLStats/type.h>
+#include <GLStats/version.h>
+
+#endif //GLSTATS_GLSTATS_H
--- GLStats/files.cmake
+++ GLStats/files.cmake
@@ -1,12 +1,12 @@
 
-# Copyright (c) 2012 Stefan Eilemann <eile@eyescale.ch>
+# Copyright (c) 2012-2013 Stefan Eilemann <eile@eyescale.ch>
 
 update_file(${CMAKE_CURRENT_SOURCE_DIR}/version.in.h
   ${OUTPUT_INCLUDE_DIR}/GLStats/version.h)
 
 
-set(GLSTATS_PUBLIC_HEADERS 
-  ${OUTPUT_INCLUDE_DIR}/GLStats/version.h 
+set(GLSTATS_PUBLIC_HEADERS
+  ${OUTPUT_INCLUDE_DIR}/GLStats/version.h
   api.h
   data.h
   entity.h
@@ -15,6 +15,7 @@ set(GLSTATS_PUBLIC_HEADERS
   thread.h
   type.h
   types.h
+  GLStats.h
   )
 
 set(GLSTATS_SOURCES
