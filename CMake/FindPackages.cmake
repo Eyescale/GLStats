c6bdcbc2043040db26a1950baa461d6a07f7db2d
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-11-01 13:36:32 +0100

    Add saucy to ppa list

--- CMake/FindPackages.cmake
+++ CMake/FindPackages.cmake
@@ -3,7 +3,13 @@
 include(System)
 list(APPEND FIND_PACKAGES_DEFINES ${SYSTEM})
 
-find_package(Lunchbox 1.8.0 REQUIRED)
+find_package(Lunchbox 1.8.0  REQUIRED)
+find_package(OpenGL   REQUIRED)
+
+if(EXISTS ${CMAKE_SOURCE_DIR}/CMake/FindPackagesPost.cmake)
+  include(${CMAKE_SOURCE_DIR}/CMake/FindPackagesPost.cmake)
+endif()
+
 if(Lunchbox_FOUND)
   set(Lunchbox_name Lunchbox)
 endif()
@@ -12,13 +18,13 @@ if(LUNCHBOX_FOUND)
 endif()
 if(Lunchbox_name)
   list(APPEND FIND_PACKAGES_DEFINES GLSTATS_USE_LUNCHBOX)
+  set(FIND_PACKAGES_FOUND "${FIND_PACKAGES_FOUND} Lunchbox")
   link_directories(${${Lunchbox_name}_LIBRARY_DIRS})
   if(NOT "${${Lunchbox_name}_INCLUDE_DIRS}" MATCHES "-NOTFOUND")
     include_directories(${${Lunchbox_name}_INCLUDE_DIRS})
   endif()
 endif()
 
-find_package(OpenGL  REQUIRED)
 if(OpenGL_FOUND)
   set(OpenGL_name OpenGL)
 endif()
@@ -27,18 +33,14 @@ if(OPENGL_FOUND)
 endif()
 if(OpenGL_name)
   list(APPEND FIND_PACKAGES_DEFINES GLSTATS_USE_OPENGL)
+  set(FIND_PACKAGES_FOUND "${FIND_PACKAGES_FOUND} OpenGL")
   link_directories(${${OpenGL_name}_LIBRARY_DIRS})
   if(NOT "${${OpenGL_name}_INCLUDE_DIRS}" MATCHES "-NOTFOUND")
     include_directories(${${OpenGL_name}_INCLUDE_DIRS})
   endif()
 endif()
 
-
-if(EXISTS ${CMAKE_SOURCE_DIR}/CMake/FindPackagesPost.cmake)
-  include(${CMAKE_SOURCE_DIR}/CMake/FindPackagesPost.cmake)
-endif()
-
-set(GLSTATS_BUILD_DEBS libavahi-compat-libdnssd-dev;libboost-regex-dev;libboost-serialization-dev;libgl1-mesa-dev;libhwloc-dev;libx11-dev)
+set(GLSTATS_BUILD_DEBS autoconf;automake;cmake;git;git-svn;libavahi-compat-libdnssd-dev;libboost-regex-dev;libboost-serialization-dev;libgl1-mesa-dev;libhwloc-dev;libx11-dev;pkg-config;subversion)
 
 set(GLSTATS_DEPENDS Lunchbox;OpenGL)
 
@@ -57,10 +59,10 @@ file(WRITE ${DEFINES_FILE_IN}
   "#define ${CMAKE_PROJECT_NAME}_DEFINES_${SYSTEM}_H\n\n")
 file(WRITE ${OPTIONS_CMAKE} "# Optional modules enabled during build\n")
 foreach(DEF ${FIND_PACKAGES_DEFINES})
-  add_definitions(-D${DEF})
+  add_definitions(-D${DEF}=1)
   file(APPEND ${DEFINES_FILE_IN}
   "#ifndef ${DEF}\n"
-  "#  define ${DEF}\n"
+  "#  define ${DEF} 1\n"
   "#endif\n")
 if(NOT DEF STREQUAL SYSTEM)
   file(APPEND ${OPTIONS_CMAKE} "set(${DEF} ON)\n")
@@ -71,3 +73,17 @@ file(APPEND ${DEFINES_FILE_IN}
 
 include(UpdateFile)
 update_file(${DEFINES_FILE_IN} ${DEFINES_FILE})
+if(Boost_FOUND) # another WAR for broken boost stuff...
+  set(Boost_VERSION ${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION})
+endif()
+if(CUDA_FOUND)
+  string(REPLACE "-std=c++11" "" CUDA_HOST_FLAGS "${CUDA_HOST_FLAGS}")
+  string(REPLACE "-std=c++0x" "" CUDA_HOST_FLAGS "${CUDA_HOST_FLAGS}")
+endif()
+if(FIND_PACKAGES_FOUND)
+  if(MSVC)
+    message(STATUS "Configured with ${FIND_PACKAGES_FOUND}")
+  else()
+    message(STATUS "Configured with ${CMAKE_BUILD_TYPE}${FIND_PACKAGES_FOUND}")
+  endif()
+endif()
